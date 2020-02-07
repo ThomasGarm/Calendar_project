b94a14d1836e24cc2186b58b1f67188ae9e59129
python <thomas.win@gmail.com>
2020-02-07 15:17:26 +0100

    main work in progress

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import locale
 import os
 from datetime import datetime
 from view.display_model import Display_model
-dp = Display_model()
+dm = Display_model()
 
 locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')
 currentYear = datetime.now().year
@@ -12,20 +12,29 @@ print (calendar.month(currentYear, currentMonth, 1, 2))
 
 
 print("Vous êtes sur votre agenda personnel.")
-if __name__ == "__main__":
- choice = ""
- while choice != "q":
-    if choice="n":
+
+choice = input("Tapez N pour suivant, tapez P pour précédent. \n Tapez A pour ajouter un RDV, tapez S pour supprimer.\n Tapez V pour afficher un RDV et B pour tout les RDV du jour: ").lower()
+while choice != "q":
+    if choice == "n":
         if currentMonth < 12:
-        currentMonth += 1
+            currentMonth += 1
         else:
-        currentMonth = 1
-        currentYear += 1
-    elif choice = "p":
-        if currentMonth > 1:
+            currentMonth = 1
+            currentYear += 1
+    elif choice == "p":
+        if currentMonth > 12:
             currentMonth -= 1
         else:
-            currentMonth = 12:
+            currentMonth = 1
             currentYear -=1
+    elif choice == "a":
+        dm.display_add_rdv()
+    elif choice == "s":
+        dm.display_del_rdv()
+    elif choice == "v":
+        dm.display_rdv
+    else:
+        dm.display_all_rdv
+exit()
 
 
